2bf7aba2dd9539fc6222227844d7a11e6d887e16
shinyaokubo-ai <shinyatatsumiya@gmail.com>
2026-03-05 14:00:21 +0900

    Add google verification

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include  # include を追加
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 
@@ -11,6 +12,9 @@ urlpatterns = [
     # webアプリのURLを読み込む
     path('', include('web.urls')),
 ]
+
+
+path('google572018d7ddc4e357.html', TemplateView.as_view(template_name='googleXXXXXXXX.html', content_type='text/html')),
 
 # 画像を表示するための設定（開発モード用）
 if settings.DEBUG:
