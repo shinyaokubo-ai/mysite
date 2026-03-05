429524aa05a173aaa4ad94d6ae1101b90563084f
shinyaokubo-ai <shinyatatsumiya@gmail.com>
2026-03-05 15:28:27 +0900

    Fix: move google path inside urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -3,18 +3,19 @@ from django.urls import path, include  # include を追加
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-
+from django.http import HttpResponse
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # webアプリのURLを読み込む
     path('', include('web.urls')),
+    
+    # ここに追加！カッコの内側に入れるのが正解です
+    path('google572018d7ddc4e357.html', lambda r: HttpResponse("google-site-verification: google572018d7ddc4e357.html", content_type="text/html")),
 ]
 
 
-path('google572018d7ddc4e357.html', TemplateView.as_view(template_name='google572018d7ddc4e357.html', content_type='text/html')),
 
 # 画像を表示するための設定（開発モード用）
 if settings.DEBUG:
